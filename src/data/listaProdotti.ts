8f83915a900e7358985bc40952d3bda4b20cde9a
Utente <giorgianieli@gmail.com>
2026-01-12 11:28:02 +0100

    creo interfacce per i prodotti da vendere

---
+++ src/data/listaProdotti.ts
@@ -0,0 +1,41 @@
+import { Prodotti } from "../types/prodotti";
+
+export const listaProdotti: Prodotti[] = [
+    {
+    id: "olio-extravergine",
+    nome: "Olio extravergine d'oliva",
+    descrizione:
+      "Olio biologico ottenuto da olive coltivate nei campi di Gianni, secondo tradizione siciliana.",
+    origine: "Sicilia",
+    prezzo: "0.05 ETH",
+    immagine: "https://placehold.co/300x200",
+  },
+  {
+    id: "olio-extravergine",
+    nome: "Olio extravergine d'oliva",
+    descrizione:
+      "Olio biologico ottenuto da olive coltivate nei campi di Gianni, secondo tradizione siciliana.",
+    origine: "Sicilia",
+    prezzo: "0.05 ETH",
+    immagine: "https://placehold.co/300x200",
+  },
+  {
+    id: "olio-extravergine",
+    nome: "Olio extravergine d'oliva",
+    descrizione:
+      "Olio biologico ottenuto da olive coltivate nei campi di Gianni, secondo tradizione siciliana.",
+    origine: "Sicilia",
+    prezzo: "0.05 ETH",
+    immagine: "https://placehold.co/300x200",
+  },
+  {
+    id: "olio-extravergine",
+    nome: "Olio extravergine d'oliva",
+    descrizione:
+      "Olio biologico ottenuto da olive coltivate nei campi di Gianni, secondo tradizione siciliana.",
+    origine: "Sicilia",
+    prezzo: "0.05 ETH",
+    immagine: "https://placehold.co/300x200",
+  },
+
+];
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,9 @@
+import { listaProdotti } from "../data/listaProdotti";
+
+
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    
+  )
 }
