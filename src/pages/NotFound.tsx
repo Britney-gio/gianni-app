53ce355dbe48e31666b10e7861800df7539715a5
Utente <giorgianieli@gmail.com>
2026-01-18 11:13:36 +0100

    Sistemo pagina NotFound nel caso di errori

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,21 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import "../styles/home.scss";
+
 export default function NotFound() {
-  return <h1>Pagina non trovata</h1>;
-}
+  const navigate = useNavigate();
+  return (
+  <>
+  
+  <div className="page">
+    <h1>Pagina non trovata</h1>
+    <button
+        className="alert-button"
+        type="button"
+        onClick={() => navigate("/")}
+      >
+        Home
+      </button>
+  </div>
+  </>
+)};
