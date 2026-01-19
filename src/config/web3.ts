2f078a9ce9b85aee0d9a5276364313bd9eb2aff2
Utente <giorgianieli@gmail.com>
2026-01-19 18:39:15 +0100

    cerco di far funzionare wagmi

--- src/config/web3.ts
+++ src/config/web3.ts
@@ -1,31 +1,20 @@
-import { WagmiProvider, createConfig } from "wagmi";
 import { sepolia } from "wagmi/chains";
 import { QueryClient } from "@tanstack/react-query";
-import { createAppKit } from "@reown/appkit";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 
 export const queryClient = new QueryClient();
 
+const projectId = import.meta.env.VITE_REOWN_PROJECT_ID as string;
+
+if (!projectId) {
+  console.log("ENV:", import.meta.env);
+  throw new Error("Missing VITE_REOWN_PROJECT_ID in .env");
+}
+
 export const wagmiAdapter = new WagmiAdapter({
   networks: [sepolia],
-  projectId: import.meta.env
-    .VITE_REOWN_PROJECT_ID83cc208cdd2d492921e97a5c8f32a128,
+  projectId,
 });
 
-export const wagmiConfig = createConfig({
-  chains: [sepolia],
-  transports: wagmiAdapter.transports,
-});
+export const wagmiConfig = wagmiAdapter.wagmiConfig;
 
-createAppKit({
-  adapters: [wagmiAdapter],
-  networks: [sepolia],
-  projectId: import.meta.env
-    .VITE_REOWN_PROJECT_ID83cc208cdd2d492921e97a5c8f32a128,
-  metadata: {
-    name: "gianni-app",
-    description: "E-commerce agricolo Web3",
-    url: "http://localhost:5173",
-    icons: [],
-  },
-});
