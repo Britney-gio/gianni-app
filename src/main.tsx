abdb713e12f92acbbb5e101f96475b3a3006fa3d
Utente <giorgianieli@gmail.com>
2026-01-19 15:10:25 +0100

    Configurazione main.tsx per wagmi e il necessario

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { WagmiProvider } from "wagmi";
+import { QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
+import { wagmiConfig, queryClient } from "./config/web3.ts";
 import "./index.css";
 import "./styles/_base.scss";
 import "./styles/media-query.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>,
 );
-
